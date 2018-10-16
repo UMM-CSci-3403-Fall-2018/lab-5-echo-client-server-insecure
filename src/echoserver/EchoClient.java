7fbc8304bbf7bc2b208694c2c85d50f12694e9aa
M1NN <score@morris.umn.edu>
2018-10-16 11:26:37 -0500

    Start work on the EchoClient file - File currently gets stuck in an infinite loop, need to fix that

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,4 +1,56 @@
 package echoserver;
 
+import java.net.*;
+import java.io.*;
+
 public class EchoClient {
+  public static final int portNumber = 6013;
+
+public static void main(String[] args) throws IOException {
+  String server;
+  // Use "127.0.0.1", i.e., localhost, if no server is specified.
+  if (args.length == 0) {
+    server = "127.0.0.1";
+  } else {
+    server = args[0];
+  }
+
+  try {
+    // Connect to the server
+    Socket socket = new Socket(server, portNumber);
+    System.out.println("Connected to socket");
+
+    // Get the input stream so we can read from that socket
+    OutputStream output = socket.getOutputStream();
+    System.out.println("Initalize output stream");
+
+    // Print all the input we receive from the server
+    int byte1;
+    while ((byte1 = System.in.read()) != -1) {
+      output.write(byte1);
+      output.flush();
+    }
+    
+
+    System.out.println("Wrote output stream");
+
+    InputStream input = socket.getInputStream();
+
+    int byte2;
+    while ((byte2 = input.read()) != -1) {
+      System.out.println(byte2);
+    }
+
+    // Close the socket when we're done reading from it
+    socket.close();
+
+  // Provide some minimal error handling.
+  } catch (ConnectException ce) {
+    System.out.println("We were unable to connect to " + server);
+    System.out.println("You should make sure the server is running.");
+  } catch (IOException ioe) {
+    System.out.println("We caught an unexpected exception");
+    System.err.println(ioe);
+  }
+}
 }
