b7dc716413536e1642b62d83ae99423add430cca
M1NN <score@morris.umn.edu>
2018-10-18 10:52:59 -0500

    EchoClient handles text correctly.

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -18,28 +18,26 @@ public static void main(String[] args) throws IOException {
   try {
     // Connect to the server
     Socket socket = new Socket(server, portNumber);
-    System.out.println("Connected to socket");
+    //System.out.println("Connected to socket");
 
     // Get the input stream so we can read from that socket
     OutputStream output = socket.getOutputStream();
-    System.out.println("Initalize output stream");
+    InputStream input = socket.getInputStream();
+    //System.out.println("Initalize output stream");
 
     // Print all the input we receive from the server
     int byte1;
+    int byte2;
     while ((byte1 = System.in.read()) != -1) {
       output.write(byte1);
       output.flush();
+      byte2 = input.read();
+      System.out.write(byte2);
+      // System.out.print((char)byte2);
     }
-    
 
-    System.out.println("Wrote output stream");
 
-    InputStream input = socket.getInputStream();
-
-    int byte2;
-    while ((byte2 = input.read()) != -1) {
-      System.out.println(byte2);
-    }
+    //System.out.println("We should be done!");
 
     // Close the socket when we're done reading from it
     socket.close();
