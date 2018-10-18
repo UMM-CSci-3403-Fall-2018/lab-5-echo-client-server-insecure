df966bf0a26b481344c6cf4c62b0b4263eb9e2b3
M1NN <score@morris.umn.edu>
2018-10-18 11:17:11 -0500

    Start and finish work on EchoServer.java

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,44 @@
 package echoserver;
 
+
+import java.net.*;
+import java.io.*;
+
 public class EchoServer {
+
+  public static final int portNumber = 6013;
+
+  public static void main(String[] args) {
+   try {
+     // Start listening on the specified port
+     ServerSocket sock = new ServerSocket(portNumber);
+     int serverByte;
+     // Run forever, which is common for server style services
+     while (true) {
+       // Wait until someone connects, thereby requesting a date
+       Socket client = sock.accept();
+       System.out.println("Got a request!");
+
+       // Construct a writer so we can write to the socket, thereby
+       // sending something back to the client.
+       InputStream input = client.getInputStream();
+       OutputStream output = client.getOutputStream();
+
+
+       while ((serverByte = input.read()) != -1) {
+         output.write(serverByte);
+         output.flush();
+       }
+
+       output.flush();
+
+       // Close the client socket since we're done.
+       client.close();
+     }
+   // *Very* minimal error handling.
+   } catch (IOException ioe) {
+     System.out.println("We caught an unexpected exception");
+     System.err.println(ioe);
+   }
+ }
 }
